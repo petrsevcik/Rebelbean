2b798a22c94caf6c3f7a27246541170537e86a17
petr.sevcik <petr.sevcik@kiwi.com>
2022-06-19 16:04:47 +0200

    feat: send email working, decoupling secrets

---
+++ emailing.py
@@ -0,0 +1,25 @@
+import smtplib
+from decouple import config
+from email.message import EmailMessage
+
+SENDER_EMAIL = config("sender_email")
+EMAIL_PASSWORD = config("email_password")
+SERVER = config("server")
+RECEIVER_EMAIL = config("receiver_email")
+
+def send_email(message):
+    sender_email = SENDER_EMAIL  # fill your email
+    receiver_email = [RECEIVER_EMAIL] #recipients
+    password = EMAIL_PASSWORD  #fill your password
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = 'Rebelbean Alert'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    server = smtplib.SMTP_SSL(SERVER, 465) #choose your provider server
+    server.login(sender_email, password)
+    server.send_message(msg)
+    server.quit()
+    return "Email sent!"
+
+print(send_email("Shreeeeed"))
